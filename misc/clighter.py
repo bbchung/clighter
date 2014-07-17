c07e8873977b5564095cd0a55eebfc4a651f2022
bbchung <afafaf4@gmail.com>
2014-07-17 20:00:39 +0800

    fix wrong ref hilighting, add priority in matchaddpos

--- misc/clighter.py
+++ misc/clighter.py
@@ -59,18 +59,26 @@ def try_highlight(vim_cursor_line, vim_cursor_col):
     if int(vim.eval("s:cursor_decl_ref_hl_on")) == 1:
         decl_ref_cursor = cindex.Cursor.from_location(gTranslationUnit[0], cindex.SourceLocation.from_position(gTranslationUnit[0], file, vim_cursor_line, vim_cursor_col)) # cusor under vim-cursor
 
-    do_highlight(window_tokens, decl_ref_cursor)
+    do_highlight(window_tokens, decl_ref_cursor, file)
 
 
-def do_highlight(window_tokens, decl_ref_cursor):
+def do_highlight(window_tokens, decl_ref_cursor, file):
     vim.command('call s:clear_match("cursor_decl_ref")')
     ref_spelling = None
     
-    if decl_ref_cursor is not None:
-        if decl_ref_cursor.kind == cindex.CursorKind.DECL_REF_EXPR or decl_ref_cursor.kind == cindex.CursorKind.MEMBER_REF_EXPR or decl_ref_cursor.kind == cindex.CursorKind.MACRO_INSTANTIATION:
-            for t in decl_ref_cursor.get_tokens():
+    #print decl_ref_cursor.kind
+
+    decl_ref_cursor_def = None
+    if decl_ref_cursor is not None and (decl_ref_cursor.kind == cindex.CursorKind.DECL_REF_EXPR or decl_ref_cursor.kind == cindex.CursorKind.MEMBER_REF_EXPR or decl_ref_cursor.kind == cindex.CursorKind.MACRO_INSTANTIATION):
+        decl_ref_cursor_def = decl_ref_cursor.get_definition()
+
+        """ Do declaring highlighting'
+        """
+        if decl_ref_cursor_def is not None:
+            for t in decl_ref_cursor_def.get_tokens():
                 if t.kind.value == 2:
-                    ref_spelling = t.spelling
+                    vim_match_add('cursor_decl_ref', 'CursorDeclRef', t.location.line, t.location.column, len(t.spelling), -1)
+                    break
 
     need_clear_semantic = 1
     for t in window_tokens:
@@ -81,17 +89,17 @@ def do_highlight(window_tokens, decl_ref_cursor):
             need_clear_semantic = 0
         if t.kind.value == 2:
             if t.cursor.kind == cindex.CursorKind.MACRO_INSTANTIATION:
-                vim_match_add('semantic', 'MacroInstantiation', t.location.line, t.location.column, len(t.spelling))
+                vim_match_add('semantic', 'MacroInstantiation', t.location.line, t.location.column, len(t.spelling), -2)
             elif t.cursor.kind == cindex.CursorKind.STRUCT_DECL:
-                vim_match_add('semantic', 'StructDecl', t.location.line, t.location.column, len(t.spelling))
+                vim_match_add('semantic', 'StructDecl', t.location.line, t.location.column, len(t.spelling), -2)
             elif t.cursor.kind == cindex.CursorKind.CLASS_DECL:
-                vim_match_add('semantic', 'ClassDecl', t.location.line, t.location.column, len(t.spelling))
+                vim_match_add('semantic', 'ClassDecl', t.location.line, t.location.column, len(t.spelling), -2)
             elif t.cursor.kind == cindex.CursorKind.ENUM_DECL:
-                vim_match_add('semantic', 'EnumDecl', t.location.line, t.location.column, len(t.spelling))
+                vim_match_add('semantic', 'EnumDecl', t.location.line, t.location.column, len(t.spelling), -2)
             elif t.cursor.kind == cindex.CursorKind.ENUM_CONSTANT_DECL:
-                vim_match_add('semantic', 'EnumConstantDecl', t.location.line, t.location.column, len(t.spelling))
+                vim_match_add('semantic', 'EnumConstantDecl', t.location.line, t.location.column, len(t.spelling), -2)
             elif t.cursor.kind == cindex.CursorKind.TYPE_REF:
-                vim_match_add('semantic', 'TypeRef', t.location.line, t.location.column, len(t.spelling))
+                vim_match_add('semantic', 'TypeRef', t.location.line, t.location.column, len(t.spelling), -2)
             elif t.cursor.kind == cindex.CursorKind.DECL_REF_EXPR:
                 is_call = 0
                 try:
@@ -101,23 +109,18 @@ def do_highlight(window_tokens, decl_ref_cursor):
                     pass
                     
                 if is_call == 0:
-                    vim_match_add('semantic', 'DeclRefExpr', t.location.line, t.location.column, len(t.spelling))
+                    vim_match_add('semantic', 'DeclRefExpr', t.location.line, t.location.column, len(t.spelling), -2)
 
             """ Do reference highlighting'
             """
-            if ref_spelling is not None and t.spelling == ref_spelling and (t.cursor.kind == cindex.CursorKind.DECL_REF_EXPR or t.cursor.kind == cindex.CursorKind.MEMBER_REF_EXPR or t.cursor.kind == cindex.CursorKind.MACRO_INSTANTIATION):
-                vim_match_add('cursor_decl_ref', 'CursorDeclRef', t.location.line, t.location.column, len(t.spelling))
-
-    """ Do declaring highlighting'
-    """
-    if decl_ref_cursor is not None:
-        def_cursor = decl_ref_cursor.get_definition()
-        if def_cursor is not None:
-            for t in def_cursor.get_tokens():
-                if t.kind.value == 2:
-                    vim_match_add('cursor_decl_ref', 'CursorDeclRef', t.location.line, t.location.column, len(t.spelling))
+            if decl_ref_cursor_def is not None:
+                t_cursor = cindex.Cursor.from_location(gTranslationUnit[0], cindex.SourceLocation.from_position(gTranslationUnit[0], file, t.location.line, t.location.column)) # cusor under vim-cursor
+                if t_cursor is not None:
+                    t_cursor_def = t_cursor.get_definition()
+                    if t_cursor_def is not None and t_cursor_def.location.line == decl_ref_cursor_def.location.line and t_cursor_def.location.column == decl_ref_cursor_def.location.column:
+                        vim_match_add('cursor_decl_ref', 'CursorDeclRef', t.location.line, t.location.column, len(t.spelling), -1)
 
 
-def vim_match_add(type, group, line, col, len):
-    vim.command("call add(w:highlight_dict['{0}'], matchaddpos('{1}', [[{2}, {3}, {4}]], -1))".format(type, group, line, col, len))
+def vim_match_add(type, group, line, col, len, priority):
+    vim.command("call add(w:highlight_dict['{0}'], matchaddpos('{1}', [[{2}, {3}, {4}]], {5}))".format(type, group, line, col, len, priority))
     # vim.command("call add(w:semantic_list, matchadd('{0}', '\%{1}l\%>{2}c.\%<{3}c', -1))".format(group, t.location.line, t.location.column-1, t.location.column+len(t.spelling) + 1));
