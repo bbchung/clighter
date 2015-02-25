956d58d53aa6ac4639280e90d2706e39eff6df27
bbchung <afafaf4@gmail.com>
2015-02-25 21:59:48 +0800

    fix bug and improve performance

--- misc/highlighting.py
+++ misc/highlighting.py
@@ -7,117 +7,6 @@ import clang_service
 SYMBOL_REF_PRI = -11
 SYNTAX_PRI = -12
 
-
-def clear_highlight():
-    __vim_clear_match_pri(SYMBOL_REF_PRI, SYNTAX_PRI)
-    highlight_window.syntactic_range = None
-    highlight_window.symbol = None
-
-
-def clear_symbol_hl():
-    __vim_clear_match_pri(SYMBOL_REF_PRI)
-    highlight_window.symbol = None
-
-
-def highlight_window(clang_service, do_symbol_hl):
-    cc = clang_service.get_cc(vim.current.buffer.name)
-    if cc is None:
-        return
-
-    parse_tick = cc.parse_tick
-
-    tu = cc.current_tu
-    if tu is None:
-        return
-
-    top = string.atoi(vim.eval("line('w0')"))
-    bottom = string.atoi(vim.eval("line('w$')"))
-
-    draw_syntax = False
-    symbol = None
-
-    if vim.current.window.vars['hl_tick'] < parse_tick \
-            or highlight_window.syntactic_range is None \
-            or top < highlight_window.syntactic_range[0] \
-            or bottom > highlight_window.syntactic_range[1]:
-        draw_syntax = True
-        __vim_clear_match_pri(SYNTAX_PRI)
-        vim.current.window.vars['hl_tick'] = parse_tick
-
-    file = tu.get_file(cc.name)
-    if vim.eval('g:ClighterCursorHL') == '1':
-        vim_cursor = clighter_helper.get_vim_cursor(tu, file)
-        symbol = clighter_helper.get_vim_symbol(vim_cursor)
-
-        if highlight_window.symbol and (not symbol or symbol != highlight_window.symbol):
-            clear_symbol_hl()
-
-        if do_symbol_hl:
-            highlight_window.symbol = symbol
-
-    if not draw_syntax and not highlight_window.symbol:
-        return
-
-    target_range = [top, bottom]
-    height = bottom - top + 1
-
-    if draw_syntax:
-        buflinenr = len(vim.current.buffer)
-        target_range = [
-            max(top - height, 1),
-            min(bottom + height, buflinenr)
-        ]
-        highlight_window.syntactic_range = target_range
-
-    location1 = cindex.SourceLocation.from_position(
-        tu, file, line=target_range[0], column=1)
-    location2 = cindex.SourceLocation.from_position(
-        tu, file, line=target_range[1] + 1, column=1)
-    tokens = tu.get_tokens(
-        extent=cindex.SourceRange.from_locations(
-            location1,
-            location2))
-
-    for t in tokens:
-        """ Do semantic highlighting'
-        """
-        if t.kind.value != 2:
-            continue
-
-        t_cursor = cindex.Cursor.from_location(
-            tu,
-            cindex.SourceLocation.from_position(
-                tu, file,
-                t.location.line,
-                t.location.column
-            )
-        )
-
-        if draw_syntax:
-            __draw_token(
-                line=t.location.line,
-                col=t.location.column,
-                len=len(t.spelling),
-                cursor_kind=t_cursor.kind,
-                type_kind=t_cursor.type.kind
-            )
-
-        """ Do definition/reference highlighting'
-        """
-        if highlight_window.symbol and t.location.line >= top and t.location.line <= bottom:
-            t_symbol = clighter_helper.get_semantic_symbol(t_cursor)
-            if t_symbol and t.spelling == t_symbol.spelling and t_symbol == highlight_window.symbol:
-                __vim_matchaddpos(
-                    group='clighterCursorSymbolRef',
-                    line=t.location.line,
-                    col=t.location.column,
-                    len=len(t.spelling),
-                    priority=SYMBOL_REF_PRI
-                )
-
-highlight_window.syntactic_range = None
-highlight_window.symbol = None
-
 group_map = {
     cindex.CursorKind.MACRO_INSTANTIATION: 'clighterMacroInstantiation',
         cindex.CursorKind.STRUCT_DECL: 'clighterStructDecl',
@@ -135,6 +24,126 @@ group_map = {
             cindex.TypeKind.FUNCTIONPROTO: 'clighterDeclRefExprCall'
         }
 }
+
+
+def clear_highlight():
+    __vim_clear_match_pri(SYMBOL_REF_PRI, SYNTAX_PRI)
+    hl_window.syntax_range = None
+    hl_window.symbol_range = None
+    hl_window.symbol = None
+
+
+def clear_symbol_hl():
+    __vim_clear_match_pri(SYMBOL_REF_PRI)
+    hl_window.symbol_range = None
+    hl_window.symbol = None
+
+
+def hl_window(clang_service, do_symbol_hl):
+    cc = clang_service.get_cc(vim.current.buffer.name)
+    if cc is None:
+        return
+
+    parse_tick = cc.parse_tick
+
+    tu = cc.current_tu
+    if tu is None:
+        return
+
+    file = tu.get_file(cc.name)
+
+    top = string.atoi(vim.eval("line('w0')"))
+    bottom = string.atoi(vim.eval("line('w$')"))
+    height = bottom - top + 1
+
+    symbol = None
+
+    if vim.eval('g:ClighterCursorHL') == '1':
+        vim_cursor = clighter_helper.get_vim_cursor(tu, file)
+        symbol = clighter_helper.get_vim_symbol(vim_cursor)
+
+    syntax_range = [max(top - height, 1), min(
+        bottom + height, len(vim.current.buffer))]
+    symbol_range = [top, bottom]
+
+    if vim.current.window.vars['hl_tick'] < parse_tick:
+        clear_highlight()
+    else:
+        if not __is_contained_in([top, bottom], hl_window.syntax_range):
+            __vim_clear_match_pri(SYNTAX_PRI)
+        else:
+            syntax_range = None
+
+        if not __is_contained_in([top, bottom], hl_window.symbol_range) or (hl_window.symbol and (not symbol or symbol != hl_window.symbol)):
+            clear_symbol_hl()
+        else:
+            symbol_range = None
+
+    if not do_symbol_hl:
+        symbol_range = None
+
+    __do_highlight(tu, file, syntax_range, symbol, symbol_range)
+    vim.current.window.vars['hl_tick'] = parse_tick
+
+hl_window.syntax_range = None
+hl_window.symbol_range = None
+hl_window.symbol = None
+
+
+def __do_highlight(tu, file, syntax_range, symbol, symbol_range):
+    if not syntax_range and (not symbol or not symbol_range):
+        return
+
+    if syntax_range:
+        hl_window.syntax_range = syntax_range
+
+    if symbol_range and symbol:
+        hl_window.symbol_range = symbol_range
+        hl_window.symbol = symbol
+
+    union_range = __union(syntax_range, symbol_range)
+
+    location1 = cindex.SourceLocation.from_position(
+        tu, file, line=union_range[0], column=1)
+    location2 = cindex.SourceLocation.from_position(
+        tu, file, line=union_range[1] + 1, column=1)
+    tokens = tu.get_tokens(
+        extent=cindex.SourceRange.from_locations(
+            location1,
+            location2))
+
+    for t in tokens:
+        if t.kind.value != 2:
+            continue
+
+        t_cursor = cindex.Cursor.from_location(
+            tu,
+            cindex.SourceLocation.from_position(
+                tu, file,
+                t.location.line,
+                t.location.column
+            )
+        )
+
+        if syntax_range:
+            __draw_token(
+                line=t.location.line,
+                col=t.location.column,
+                len=len(t.spelling),
+                cursor_kind=t_cursor.kind,
+                type_kind=t_cursor.type.kind
+            )
+
+        if symbol and symbol_range:
+            t_symbol = clighter_helper.get_semantic_symbol(t_cursor)
+            if t_symbol and t.spelling == t_symbol.spelling and t_symbol == symbol:
+                __vim_matchaddpos(
+                    group='clighterCursorSymbolRef',
+                    line=t.location.line,
+                    col=t.location.column,
+                    len=len(t.spelling),
+                    priority=SYMBOL_REF_PRI
+                )
 
 
 def __draw_token(line, col, len, cursor_kind, type_kind):
@@ -161,3 +170,30 @@ def __vim_matchaddpos(group, line, col, len, priority):
 def __vim_clear_match_pri(*priorities):
     cmd = "call s:clear_match_pri({0})"
     vim.command(cmd.format(list(priorities)))
+
+
+def __union(range1, range2):
+    if range1 and range2:
+        return [min(range1[0], range2[0]), max(range1[1], range2[1])]
+    elif range1 and not range2:
+        return range1
+    elif not range1 and range2:
+        return range2
+    else:
+        return None
+
+
+def __is_contained_in(range1, range2):
+    if not range1:
+        return True
+
+    if not range2:
+        return False
+
+    if range1[0] < range2[0]:
+        return False
+
+    if range1[1] > range2[1]:
+        return False
+
+    return True
