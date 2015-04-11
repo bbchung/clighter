290a310a9a8132f9628c9579b3c3f1d527056d17
bbchung <afafaf4@gmail.com>
2015-04-11 13:09:43 +0800

    add cursor info column in ClighterShowInfo command

--- misc/clighter.py
+++ misc/clighter.py
@@ -42,9 +42,27 @@ def update_buffer_if_allow():
               string.atoi(vim.eval('b:changedtick')))])
 
 
+def get_vim_cursor_info():
+    cc = clang_service.ClangService().get_cc(vim.current.buffer.name)
+    if not cc:
+        return None
+
+    tu = cc.current_tu
+    if not tu:
+        return None
+
+    vim_cursor = clighter_helper.get_vim_cursor(tu, tu.get_file(cc.name))
+
+    if vim_cursor:
+        return vim_cursor.kind, vim_cursor.type.kind
+    else:
+        return None
+
+
 def show_information():
     print "Enable clighter: %s" % ('Enable' if vim.eval('s:clighter_enabled') == '1' else 'Disable')
     print "Current context: %s" % (clang_service.ClangService().current_cc.name if clang_service.ClangService().current_cc else '')
     print "Highlight occurrences: %s" % ('On' if vim.eval('g:ClighterOccurrences') == '1' else 'Off')
     print "Compilation database: %s" % ((clang_service.ClangService().compilation_database.file_path) if clang_service.ClangService().compilation_database else None)
     print "Compile args: %s" % (' '.join(clang_service.ClangService().current_cc.compile_args) if clang_service.ClangService().current_cc and clang_service.ClangService().current_cc.compile_args else '')
+    print 'Cursor info:', get_vim_cursor_info()
